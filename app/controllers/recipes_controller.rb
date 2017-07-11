639e0ed1cb12ea71f2d9a7fc24da15b6ce55bb00
Justin Joyce <justinwjoyce@gmail.com>
2017-07-10 20:01:51 -0500

    adding show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
 	def index
-		
+		@recipe = Recipe.all.order("created_at DESC")
 	end
 
 	def show
@@ -14,7 +14,13 @@ class RecipesController < ApplicationController
 	end
 
 	def create
-		@recipe = Recipe.new(recipe_params)
+		@recipe = current_user.recipes.build(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe, notice: "Successfully created new recipe"
+		else
+			render 'new'
+		end
 	end
 
 	private
